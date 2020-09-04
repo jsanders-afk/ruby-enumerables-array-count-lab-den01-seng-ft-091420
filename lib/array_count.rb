92d61b4a052bc6cbde832dbb42ac0f7e2969848c
Jack Sanders <guitarbreakerjs@gmail.com>
2020-09-04 05:12:59 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,4 +1,11 @@
-def count_strings(array)
+array = [0, 1, -9, 24, 5, -10]
+
+array2 = [3, "hey", -9, 24, "string here", -10]
+
+
+def count_strings(array2)
+  array.count do |element|
+  element.class == String
   # Return the total number of strings in the provided array using the count enumerable
 end
 
